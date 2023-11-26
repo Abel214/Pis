11ff4b4fcb7efca41097049104edb92a55ec7ab2
Elia700 <elias.poma@unl.edu.ec>
2023-11-26 18:27:52 -0500

    Controlador docentes mas atributos

--- Pis/src/Docente/Controlador/DocenteControl.java
+++ Pis/src/Docente/Controlador/DocenteControl.java
@@ -4,10 +4,58 @@
  */
 package Docente.Controlador;
 
+import Docente.Modelo.Docente;
+import controlador.listas.DynamicList;
+
 /**
  *
  * @author ALEJANDRO
  */
 public class DocenteControl {
+    private Docente docente;
+    private DynamicList<Docente> docentes;
+     public DocenteControl() {
+        docente = new Docente();
+        this.docentes = new DynamicList<>();
+    }
+
+
+    public Docente getDocente() {
+        if(docente==null){
+            docente=new Docente();
+        }
+            
+        return docente;
+    }
+
+    public void setVendedor(Docente docente) {
+        this.docente = docente;
+    }
+
+    public DynamicList<Docente> getDocentes() {
+        return docentes;
+    }
+
+    public void setDocentes(DynamicList<Docente> docentes) {
+        this.docentes = docentes;
+    }
+
     
+
+    
+    //Mwtodo guardar
+    public Boolean guardar(){
+        docente.setId(generarId());
+        docentes.add(docente);
+        return true;
+    }
+    private Integer generarId(){
+        return docentes.getLength()+1;
+  }
+    
+   
+    
+     public void imprimir(){
+        System.out.println(docentes.toString());
+    }
 }
