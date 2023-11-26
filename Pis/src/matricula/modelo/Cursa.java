ff4228c0b349c7eeca3407f7990720226c50b645
Darwin Granda <darwin.granda@unl.edu.ec>
2023-11-26 11:00:27 -0500

    Clase periodo academico

--- Pis/src/matricula/modelo/Cursa.java
+++ Pis/src/matricula/modelo/Cursa.java
@@ -16,6 +16,29 @@ public class Cursa {
     private String paralelo;
     private Integer id_docente;
 
+    public Cursa(Integer id, Integer id_estudiante, Integer id_matricula, Integer id_materia, String paralelo, Integer id_docente) {
+        this.id = id;
+        this.id_estudiante = id_estudiante;
+        this.id_matricula = id_matricula;
+        this.id_materia = id_materia;
+        this.paralelo = paralelo;
+        this.id_docente = id_docente;
+    }
+
+    public Cursa() {
+        this.id = null;
+        this.id_docente = null;
+        this.id_estudiante = null;
+        this.id_materia = null;
+        this.id_matricula = null;
+        this.paralelo = null;
+    }
+    
+    
+    
+    
+    
+
     public Integer getId() {
         return id;
     }
