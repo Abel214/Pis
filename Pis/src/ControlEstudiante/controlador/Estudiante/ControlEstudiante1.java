5a6f2ccae16ea3e293aad49e5f0835bd0e20b2f8
Abel214 <abelmoral@gmail.com>
2023-11-25 20:10:19 -0500

    Nuevo control estudiante1(dao)

---
+++ Pis/src/ControlEstudiante/controlador/Estudiante/ControlEstudiante1.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ControlEstudiante.controlador.Estudiante;
+
+import ControlEstudiante.modelo.Estudiante;
+import controlador.dao.DaoImplement;
+import controlador.listas.DynamicList;
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class ControlEstudiante1 extends DaoImplement<Estudiante>{
+     private DynamicList<Estudiante>listR=new DynamicList<>();
+     private Estudiante estudiante;
+     
+     public ControlEstudiante1(){
+         super(Estudiante.class);
+     }
+    public DynamicList<Estudiante> getEstudiantes() {
+        return listR = all();
+    }
+
+    public void setListR(DynamicList<Estudiante> listR) {
+        this.listR = listR;
+    }
+
+    public Estudiante getEstudiante() {
+        if(estudiante==null)
+            estudiante=new Estudiante();
+            return estudiante;
+        
+        
+    }
+
+
+    public void setVendedor (Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+    public Boolean persit(){
+        estudiante.setId(all().getLength()+1);
+        
+        return persist(estudiante);
+    }
+}
