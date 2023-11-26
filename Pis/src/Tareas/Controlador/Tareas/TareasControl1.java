79ff51032adcaa93c1465860b420b87a3b542f7d
Abel214 <abelmoral@gmail.com>
2023-11-26 15:11:47 -0500

    Creación tarea 1 y so código

---
+++ Pis/src/Tareas/Controlador/Tareas/TareasControl1.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tareas.Controlador.Tareas;
+
+import Tareas.Modelo.Tarea;
+import controlador.dao.DaoImplement;
+import controlador.listas.DynamicList;
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class TareasControl1  extends DaoImplement<Tarea> {
+    private DynamicList<Tarea>listR=new DynamicList<>();
+     private Tarea tarea;
+     
+     public TareasControl1(){
+         super(Tarea.class);
+     }
+    public DynamicList<Tarea> getListR() {
+        listR=all();
+        return listR;
+    }
+
+    public void setListR(DynamicList<Tarea> listR) {
+        this.listR = listR;
+    }
+
+    public Tarea getTarea() {
+        if(tarea==null)
+            tarea=new Tarea();
+            return tarea;
+        
+        
+    }
+
+
+    public void setTarea(Tarea tarea) {
+        this.tarea=tarea;
+    }
+    public Boolean persit(){
+        tarea.setId(all().getLength()+1);
+        
+        return persist(tarea);
+    }
+}
