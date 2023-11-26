d5f0fe0cb5dd3172d50a62f033bc91fa4895169c
Darwin Granda <darwin.granda@unl.edu.ec>
2023-11-26 01:44:33 -0500

    Clase Malla y Facultad

--- Pis/src/matricula/modelo/Malla.java
+++ Pis/src/matricula/modelo/Malla.java
@@ -9,6 +9,59 @@ package matricula.modelo;
  * @author darwin
  */
 public class Malla {
+    private String nombre;
+    private Integer id;
+    private Integer numCiclos;
+
+    public Malla(String nombre, Integer id, Integer numCiclos) {
+        this.nombre = nombre;
+        this.id = id;
+        this.numCiclos = numCiclos;
+    }
+
+    public Malla() {
+        this.nombre = null;
+        this.id = null;
+        this.numCiclos = null;
+    }
+    
+    
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumCiclos() {
+        return numCiclos;
+    }
+
+    public void setNumCiclos(Integer numCiclos) {
+        this.numCiclos = numCiclos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+    
+    
+    
     
     
 }
