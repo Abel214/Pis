0e62b0f80d0d054ad6fc3c79834cfd15d54ec4eb
Elia700 <elias.poma@unl.edu.ec>
2023-11-26 00:30:35 -0500

    Corrección Util Vista

--- Pis/src/materias/Vista/Util/UtilVista.java
+++ Pis/src/materias/Vista/Util/UtilVista.java
@@ -4,10 +4,23 @@
  */
 package materias.Vista.Util;
 
+import javax.swing.JComboBox;
+import materias.Modelo.Materia;
+
 /**
  *
  * @author Elías
  */
 public class UtilVista {
+     public static void CargarComboRolesL(JComboBox cbx)throws Exception{
+        materias.Controlador.MateriaControl rc= new materias.Controlador.MateriaControl();
+        cbx.removeAllItems();
+         for (int i = 0; i < rc.getMaterias().getLength(); i++) {
+            cbx.addItem(rc.getMaterias().getInfo(i));
+        }
+    }
     
+    public static Materia ObtenerCasaControladorL(JComboBox cbx){
+        return (Materia)cbx.getSelectedItem();
+    }
 }
