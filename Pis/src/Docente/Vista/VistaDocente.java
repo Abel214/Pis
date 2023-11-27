f02f0d37c7e629018a3a181ef3bc5f8810b79059
Abel214 <abel.mora@unl.edu.ec>
2023-11-26 21:04:11 -0500

    Creación métodos vista

--- Pis/src/Docente/Vista/VistaDocente.java
+++ Pis/src/Docente/Vista/VistaDocente.java
@@ -19,6 +19,8 @@ public class VistaDocente extends javax.swing.JFrame {
      */
     public VistaDocente() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        Limpiar();
     }
     
 
